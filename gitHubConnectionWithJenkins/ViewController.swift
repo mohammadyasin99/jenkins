5e0773bd01da323beb9d1588f338bb7faccc59cf
Mohammad Yassin <myassin@madfooat.com>
2021-03-10 10:05:59 +0200

    Tesint

---
+++ gitHubConnectionWithJenkins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitHubConnectionWithJenkins
+//
+//  Created by Mohmmad Yassin on 10/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("")
+    }
+
+
+}
+
